63b135e829a260aee0e9c8af1c5c668c9fec61c2
adamgemmell <gemmell.adam+github@gmail.com>
2018-03-25 16:46:55 +0100

    Javadocs, couple small changes

--- src/main/scala/com/ajsg2/minimaljava/codegen/Utils.scala
+++ src/main/scala/com/ajsg2/minimaljava/codegen/Utils.scala
@@ -7,7 +7,7 @@ import scala.collection.JavaConverters._
 
 package object Utils {
 
-	// Stable identifiers for patterh matching
+	// Stable identifiers for pattern matching
 	final val intType: CtClass = CtClass.intType
 	final val charType = CtClass.charType
 	final val longType = CtClass.longType
@@ -20,6 +20,7 @@ package object Utils {
 	def getName(node: Node, logger: Logger): String = {
 		if (node.getNodeId != NodeType.name) {
 			logger.error("Internal error: Expected name, received " + node.getNodeId)
+			System.exit(1)
 		}
 
 		val strings = node.getData.asInstanceOf[java.util.List[String]].asScala
@@ -31,23 +32,40 @@ package object Utils {
 	// Above but not only for nodes
 	def getStructure(strings: Seq[String]): String = strings.mkString(".")
 
-	// Error if there isn't the expected number of children
+	/**
+	  * Check that a node has a certain number of children. Errors likely if this is not true.
+	  *
+	  * @param n      The node
+	  * @param i      The expected number of children
+	  * @param logger The logger to report to
+	  */
 	def assertNumChildren(n: Node, i: Int, logger: Logger): Unit = {
 		if (n.getChildren.asScala.lengthCompare(i) != 0) {
-			logger.error("Internal error: For node " + n.getType + " expected " + i +
+			logger.warn("Internal error: For node " + n.getType + " expected " + i +
 					"children, found " + n.getChildren.size())
 		}
 	}
 
-	// Error if there aren't enough children
+	/**
+	  * Check that a node has a minimum number of children. Errors likely if this is not true.
+	  *
+	  * @param n      The node
+	  * @param i      The minimum number of children
+	  * @param logger The logger to report to
+	  */
 	def assertMinChildren(n: Node, i: Int, logger: Logger): Unit = {
 		if (n.getChildren.asScala.lengthCompare(i) < 0) {
-			logger.error("Internal error: For node " + n.getType + " expected at least " + i +
+			logger.warn("Internal error: For node " + n.getType + " expected at least " + i +
 					"children, found " + n.getChildren.size())
 		}
 	}
 
-	// Longs & doubles use 2 words
+	/**
+	  * Get the number of locla variables a type takes up.
+	  *
+	  * @param t The type
+	  * @return
+	  */
 	def varSize(t: java.lang.reflect.Type): Int = if (t == java.lang.Long.TYPE ||
 			t == java.lang.Double.TYPE) {
 		2
@@ -55,6 +73,14 @@ package object Utils {
 		1
 	}
 
+	/**
+	  * Get a CtClass representing a class name
+	  *
+	  * @param className the class's name
+	  * @param cp        class pool in which to search for classes
+	  * @param logger    logger in which to report an error
+	  * @return A CtClass
+	  */
 	def getCtClass(className: String, cp: ClassPool, logger: Logger): CtClass = {
 		className match {
 			case "int" => intType
